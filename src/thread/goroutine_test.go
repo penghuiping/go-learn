630d11eb8fda44c8cc56f4431dc64290c15baa14
penghuiping <575813261@qq.com>
2019-06-03 22:28:36 +0800

    修改文件名

--- src/thread/goroutine_test.go
+++ src/thread/goroutine_test.go
@@ -17,25 +17,59 @@ func GoRoutineExample() {
 
 	go func() {
 		defer wg.Done()
+		var count = 0
 		for {
 			fmt.Println("")
 			fmt.Println("1111111")
 			fmt.Println("3333333")
 			fmt.Println("")
-			time.Sleep(1 * time.Millisecond)
+			time.Sleep(1000 * time.Millisecond)
+			count++
+			if 5 == count {
+				break;
+			}
 		}
 
 	}()
 
 	go func() {
 		defer wg.Done()
+		var count = 0
 		for {
 			fmt.Println("")
 			fmt.Println("2222222")
 			fmt.Println("4444444")
 			fmt.Println("")
-			time.Sleep(1 * time.Millisecond)
+			time.Sleep(1000 * time.Millisecond)
+			count++
+			if 5 == count {
+				break;
+			}
 		}
 	}()
 	wg.Wait()
+}
+
+func GoRoutingExample2() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "2s"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		fmt.Println(<-c1)
+		fmt.Println(<-c2)
+	}
 }
